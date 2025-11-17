e6c15bdbb51aea78912b52caff0bee99c6b218fd
Dev-ayo10 <ernest.dev10@gmail.com>
2025-11-18 00:43:14 +0100

    omooo

--- ayo-project/src/api/axiosInstance.js
+++ ayo-project/src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiBaseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+const apiBaseURL = import.meta.env.VITE_API_BASE_URL || "/api";
 
 const axiosInstance = axios.create({
   baseURL: apiBaseURL,
